b708568c60726e4cccb4cbb42f49aa38176831de
Franco_H <francoh.jobs@gmail.com>
2023-02-23 03:00:54 -0500

    fixing

--- code_templates/code_testing/script.js
+++ code_templates/code_testing/script.js
@@ -1,5 +1,13 @@
-var location = 'https://api.github.com/repos/Franco-Henriquez/Web_Fundamentals/contents/';
+var location = '/Web_Fundamentals/HTML/Practice';
 var path = '/Web_Fundamentals/HTML';
-var directoryPaths = path.substring(1, location.lastIndexOf("/")+1);
+var directoryPaths = location.substring(0, location.lastIndexOf("/"));
 var directoryName = path.substring(path.lastIndexOf("/")+1); /*current working directory*/
 console.log("Directory name: " + directoryPaths);
+
+const pathArray = [];
+pathArray.push(location.split( '/' )[2]);
+pathArray.push(location.split( '/' )[3]);
+
+
+console.log("Path Array: " + pathArray);
+console.log("Subdirectories only: " + pathArray[0] + "/" + pathArray[1] + "/");
