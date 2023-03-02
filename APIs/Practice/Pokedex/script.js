723ba976c412555c2f2af710f0d3265505b0626d
Franco_H <francoh.jobs@gmail.com>
2023-03-02 02:00:16 -0500

    height and weight undo capitalization

--- APIs/Practice/Pokedex/script.js
+++ APIs/Practice/Pokedex/script.js
@@ -24,15 +24,15 @@ async function iChooseU(){
     // log(pokeResults)
     pokeStats = document.getElementById('stats-list');
     var pokeTypes = pokeTypesCheck(pokeResults);
-    var pokeHeight = capitalize(pokeResults.height)
-    var pokeWeight = capitalize(pokeResults.weight)
+    var pokeHeight = pokeResults.height
+    var pokeWeight = pokeResults.weight
     // log(pokeStats)
     // listPokeStats(pokeStats,pokeTypes,pokemonName);
     pokeStats.innerHTML = `<li><b>Name:</b>&nbsp;<div> ${pokemonName}</div></li>`
     // await sleep(3000);
     pokeStats.innerHTML += `<li><b>Type:</b>&nbsp;<div> ${pokeTypes}</div></li>`
     pokeStats.innerHTML += `<li><b>Height:</b>&nbsp;<div> 0.${pokeHeight}m</div></li>`
-    pokeStats.innerHTML += `<li><b>Height:</b>&nbsp;<div> ${pokeWeight}.0kg</div></li>`
+    pokeStats.innerHTML += `<li><b>Weight:</b>&nbsp;<div> ${pokeWeight}.0kg</div></li>`
     spritePic.src = pokeResults.sprites.front_shiny
 
 }
